8c86ef17ce5b66c11f09b64fb18a4f3fa3ea8fa4
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2023-12-21 23:19:37 +0100

    edit title in navbar and make product-list responsive

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,18 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   inject,
   Input,
+  OnDestroy,
   OnInit,
 } from '@angular/core';
 import { ProductService } from '../../services/product/product.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Product } from '../../models/product';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { ProductItemComponent } from './product-item/product-item.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'gift-product-list',
@@ -19,12 +22,31 @@ import { ProductItemComponent } from './product-item/product-item.component';
   styleUrl: './product-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   @Input({ required: true }) shortView!: boolean;
+
   products$?: Observable<Product[]>;
+  isSmartphoneScreen = false;
+
+  private cdRef = inject(ChangeDetectorRef);
   private productService = inject(ProductService);
+  private breakpointObserver = inject(BreakpointObserver);
+  private subscription: Subscription = new Subscription();
 
   ngOnInit() {
     this.products$ = this.productService.getProducts();
+
+    //FIXME: REACT ON CHANGE SCREEN SIZE landscape or portrait
+    // BREAKPOINT CAN REACT OF landscape and portrait
+    this.subscription = this.breakpointObserver
+      .observe([Breakpoints.XSmall, Breakpoints.Small])
+      .subscribe((result) => {
+        this.isSmartphoneScreen = result.matches;
+        this.cdRef.detectChanges();
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
